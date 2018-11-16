6f2846a8214476e9df8a689c3a4d92d13b79b59e
André "MG" Wisén <andre.wisen@gmail.com>
2018-11-16 10:51:56 +0100

    Add initial source code

---
+++ getIP.php
@@ -0,0 +1,23 @@
+<?php
+// strip slashes before putting the form data into target file
+$cd = stripslashes($_POST['code']);
+
+// Show the msg, if the code string is empty
+if (empty($cd))
+    echo "Nothing to write";
+
+// if the code string is not empty then open the target file and put form data in it
+else
+{
+    $file = fopen("text.txt", "w");
+    echo fwrite($file, $cd);
+
+    // show a success msg 
+    echo "data successfully entered";
+    fclose($file);
+}
+
+header('Location:index.php');
+
+
+?>
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>live.andrewisen.se</title>
+	</head>
+
+	<body>
+		<br>
+		<?php
+
+
+			$msgA = "IP: ";
+			$msgB = "Stream: rtmp://";
+			$msgC = "/live/test";
+
+			$IP = fopen("text.txt", "r");
+			$IPaddr = fgets($IP);
+
+			// Read line from the text file and write the contents to the client
+			echo $msgA.$IPaddr."<br>";
+			echo $msgB.$IPaddr.$msgC;
+
+			fclose($IP);
+
+		?>
+
+		<br>
+		<br>
+		<!--<form action="writeIP.php"method="post">
+		<textarea name="code">Din IP</textarea>
+		<br>
+		<input type="submit"value="Uppdatera IP">
+		</form>-->
+
+		<form action="/getIP.php" method="post">
+		Nytt IP: <input type="text" name="code" placeholder="192.168.0.1"> <input type="submit" value="Submit">
+		</form>
+
+	</body>
+
+</html>
