42310b4ed95a9ae354ba5e1d17b5086a1c19b094
André "MG" Wisén <andre.wisen@gmail.com>
2018-12-10 09:16:10 +0100

    Made pretty

--- getIP.php
+++ getIP.php
@@ -1,24 +1,19 @@
 <?php
-// strip slashes before putting the form data into target file
-$cd = stripslashes($_POST['code']);
-header('Location:index.php');
+	// strip slashes before putting the form data into target file
+	$cd = stripslashes($_POST['code']);
+	header('Location:index.php');
 
-// Show the msg, if the code string is empty
-if (empty($cd))
-    echo "Nothing to write";
+	// Show the msg, if the code string is empty
+	if (empty($cd))
+	    echo "Nothing to write";
+	// if the code string is not empty then open the target file and put form data in it
+	else
+	{
+	    $file = fopen("text.txt", "w");
+	    echo fwrite($file, $cd);
 
-// if the code string is not empty then open the target file and put form data in it
-else
-{
-    $file = fopen("text.txt", "w");
-    echo fwrite($file, $cd);
-
-    // show a success msg 
-    echo "data successfully entered";
-    fclose($file);
-}
-
-
-
-
+	    // show a success msg 
+	    echo "data successfully entered";
+	    fclose($file);
+	}
 ?>
