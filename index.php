e7760135863da2b3b53ee8754325db5825b40613
André "MG" Wisén <andre.wisen@gmail.com>
2018-12-10 09:14:36 +0100

    Add comments

--- index.php
+++ index.php
@@ -1,31 +1,35 @@
 <?php
+	// Force no cache - server side
 
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Cache-Control: post-check=0, pre-check=0", false);
-header("Pragma: no-cache");
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
-		<title>live.andrewisen.se</title>
+		<title>Manage Livestream</title>
 	</head>
 
 	<body>
 		<br>
 		<?php
-
+			// Message B, and Message C concatenates into the full URL
+			// E.g. Stream: rtmp://192.168.0.1/live/test
 
 			$msgA = "IP: ";
 			$msgB = "Stream: rtmp://";
 			$msgC = "/live/test";
 
+			// The IP Address is located (in plain text) in the file "ip.txt"
+
 			$IP = fopen("text.txt", "r");
 			$IPaddr = fgets($IP);
 			$IPaddrXSS = htmlspecialchars($IPaddr, ENT_QUOTES);
 
-			// Read line from the text file and write the contents to the client
+			// Reads line from the text file and write the contents to the client
 			echo $msgA.$IPaddrXSS."<br>";
 			echo $msgB.$IPaddrXSS.$msgC;
 
@@ -34,11 +38,6 @@ header("Pragma: no-cache");
 
 		<br>
 		<br>
-		<!--<form action="writeIP.php"method="post">
-		<textarea name="code">Din IP</textarea>
-		<br>
-		<input type="submit"value="Uppdatera IP">
-		</form>-->
 
 		<form action="./getIP.php" method="post">
 		Nytt IP: <input type="text" name="code" placeholder="192.168.0.1"> <input type="submit" value="Submit">
